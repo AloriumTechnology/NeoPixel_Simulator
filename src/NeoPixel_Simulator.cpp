0070153ff21c196090f10704c55d3f73a384dc74
jason <14111408+jpecor@users.noreply.github.com>
2020-08-10 10:00:45 -0500

    Initial check-in

---
+++ src/NeoPixel_Simulator.cpp
@@ -0,0 +1,445 @@
+/*-------------------------------------------------------------------------
+  NeoPixel_Simulator is an Arduino library designed to simulate the
+  behavior of a 12x12 NeoPixel array using the Arduino serial monitor. 
+
+  Written by Bryan Craker of Alorium Technology.
+  info@aloriumtech.com
+
+  NeoPixel_Simulator is based on the Adafruit_NeoPixel library from Adafruit
+  Industries which can be found here:
+
+  https://github.com/adafruit/Adafruit_NeoPixel
+
+  -------------------------------------------------------------------------
+
+  NeoPixel_Simulator Usage:
+
+  NeoPixel_Simulator can be used by adding a define for the simulator and 
+  then adding an ifdef structure that  imports NeoPixel_Simulator and
+  redefines Adafruit_NeoPixel:
+
+  #define SIMULATE_NEOS
+
+  #ifdef SIMULATE_NEOS
+    #include <NeoPixel_Simulator.h>             // Simulator library for NeoPixels
+    #define Adafruit_NeoPixel NeoPixel_Simulator  
+  #else
+    #include <Adafruit_NeoPixel.h>             // Library for NeoPixels
+  #endif
+
+  -------------------------------------------------------------------------
+
+  NeoPixel_Simulator is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  NeoPixel_Simulator is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with NeoPixel.  If not, see
+  <http://www.gnu.org/licenses/>.
+  -------------------------------------------------------------------------*/
+
+#include "NeoPixel_Simulator.h"
+
+#if defined(NRF52)
+#include "nrf.h"
+
+// Interrupt is only disabled if there is no PWM device available
+// Note: Adafruit Bluefruit nrf52 does not use this option
+//#define NRF52_DISABLE_INT
+#endif
+
+// Constructor when length, pin and type are known at compile-time:
+NeoPixel_Simulator::NeoPixel_Simulator(uint16_t n, uint8_t p, neoPixelType t) :
+  begun(false), brightness(0), pixels(NULL), endTime(0)  
+{
+  updateType(t);
+  updateLength(n);
+  setPin(p);
+}
+
+// via Michael Vogt/neophob: empty constructor is used when strand length
+// isn't known at compile-time; situations where program config might be
+// read from internal flash memory or an SD card, or arrive via serial
+// command.  If using this constructor, MUST follow up with updateType(),
+// updateLength(), etc. to establish the strand type, length and pin number!
+NeoPixel_Simulator::NeoPixel_Simulator() :
+#ifdef NEO_KHZ400
+  is800KHz(true),
+#endif
+  begun(false), numLEDs(0), numBytes(0), pin(-1), brightness(0), pixels(NULL),
+  rOffset(1), gOffset(0), bOffset(2), wOffset(1), endTime(0)
+{
+}
+
+NeoPixel_Simulator::~NeoPixel_Simulator() {
+  if(pixels)   free(pixels);
+  if(pin >= 0) pinMode(pin, INPUT);
+}
+
+void NeoPixel_Simulator::begin(void) {
+  if(pin >= 0) {
+    //pinMode(pin, OUTPUT);
+    //digitalWrite(pin, LOW);
+  }
+  Serial.begin(115200);
+  begun = true;
+
+}
+
+void NeoPixel_Simulator::updateLength(uint16_t n) {
+  if(pixels) free(pixels); // Free existing data (if any)
+
+  // Allocate new data -- note: ALL PIXELS ARE CLEARED
+  numBytes = n * ((wOffset == rOffset) ? 3 : 4);
+  if((pixels = (uint8_t *)malloc(numBytes))) {
+    memset(pixels, 0, numBytes);
+    numLEDs = n;
+  } else {
+    numLEDs = numBytes = 0;
+  }
+}
+
+void NeoPixel_Simulator::updateType(neoPixelType t) {
+  boolean oldThreeBytesPerPixel = (wOffset == rOffset); // false if RGBW
+
+  wOffset = (t >> 6) & 0b11; // See notes in header file
+  rOffset = (t >> 4) & 0b11; // regarding R/G/B/W offsets
+  gOffset = (t >> 2) & 0b11;
+  bOffset =  t       & 0b11;
+#ifdef NEO_KHZ400
+  is800KHz = (t < 256);      // 400 KHz flag is 1<<8
+#endif
+
+  // If bytes-per-pixel has changed (and pixel data was previously
+  // allocated), re-allocate to new size.  Will clear any data.
+  if(pixels) {
+    boolean newThreeBytesPerPixel = (wOffset == rOffset);
+    if(newThreeBytesPerPixel != oldThreeBytesPerPixel) updateLength(numLEDs);
+  }
+}
+
+#if defined(ESP8266) 
+// ESP8266 show() is external to enforce ICACHE_RAM_ATTR execution
+extern "C" void ICACHE_RAM_ATTR espShow(
+  uint8_t pin, uint8_t *pixels, uint32_t numBytes, uint8_t type);
+#elif defined(ESP32)
+extern "C" void espShow(
+  uint8_t pin, uint8_t *pixels, uint32_t numBytes, uint8_t type);
+#endif // ESP8266
+
+void NeoPixel_Simulator::show(void) {
+
+  if(!pixels) return;
+
+  int iNeo;
+  uint32_t tmpColor;
+
+  // Read colors from the NeoPixel array and write equivalents to the Serial Monitor 
+  for (int yInd = (NUM_ROWS - 1); yInd >= 0; yInd--)
+  {
+    for (int xInd = 0; xInd < NUM_COLS; xInd++)
+    {
+      iNeo = yInd * NUM_COLS;
+
+      if ( (yInd % 2) == 0)
+      {   // Even row
+        iNeo = iNeo + (12 - xInd);
+      }
+      else
+      {   // Odd row
+        iNeo = iNeo + (xInd + 1);
+      }
+      tmpColor = getPixelColor(iNeo);
+
+      // Wait until the UART's TX buffer has space
+      while (Serial.availableForWrite() == 0);
+
+      // Write the color to the Serial Monitor
+      if      (tmpColor == COLOR_WHITE)           Serial.write("W ");
+      else if (tmpColor == COLOR_BLACK)           Serial.write(". ");
+      else if (tmpColor == COLOR_RED)             Serial.write("R ");
+      else if (tmpColor == COLOR_GREEN)           Serial.write("G ");
+      else if (tmpColor == COLOR_BLUE)            Serial.write("B ");
+      else if (tmpColor == COLOR_YELLOW)          Serial.write("Y ");
+      else if (tmpColor == COLOR_CYAN)            Serial.write("C ");
+      else if (tmpColor == COLOR_MAGENTA)         Serial.write("M ");
+      else if (tmpColor == COLOR_FLUSH_ORANGE)    Serial.write("o ");
+      else if (tmpColor == COLOR_ROSE)            Serial.write("r ");
+      else if (tmpColor == COLOR_CHARTREUSE)      Serial.write("c ");
+      else if (tmpColor == COLOR_SPRING_GREEN)    Serial.write("g ");
+      else if (tmpColor == COLOR_AZURE)           Serial.write("a ");
+      else if (tmpColor == COLOR_ELECTRIC_INDIGO) Serial.write("i ");
+      else                                        Serial.write("X ");   
+    }
+    Serial.write("\n");
+  }
+
+  // Wait until the UART's TX buffer has space
+  while (Serial.availableForWrite() == 0);
+
+  // Scroll simulated display screen
+  for (int bInd = 0; bInd < 5; bInd++)
+  {
+    Serial.write("\n");
+  }
+
+}
+
+// Set the output pin number
+void NeoPixel_Simulator::setPin(uint8_t p) {
+  if(begun && (pin >= 0)) pinMode(pin, INPUT);
+    pin = p;
+    if(begun) {
+      pinMode(p, OUTPUT);
+      digitalWrite(p, LOW);
+    }
+#ifdef __AVR__
+    port    = portOutputRegister(digitalPinToPort(p));
+    pinMask = digitalPinToBitMask(p);
+#endif
+}
+
+// Set pixel color from separate R,G,B components:
+void NeoPixel_Simulator::setPixelColor(
+ uint16_t n, uint8_t r, uint8_t g, uint8_t b) {
+
+  if(n < numLEDs) {
+    if(brightness) { // See notes in setBrightness()
+      r = (r * brightness) >> 8;
+      g = (g * brightness) >> 8;
+      b = (b * brightness) >> 8;
+    }
+    uint8_t *p;
+    if(wOffset == rOffset) { // Is an RGB-type strip
+      p = &pixels[n * 3];    // 3 bytes per pixel
+    } else {                 // Is a WRGB-type strip
+      p = &pixels[n * 4];    // 4 bytes per pixel
+      p[wOffset] = 0;        // But only R,G,B passed -- set W to 0
+    }
+    p[rOffset] = r;          // R,G,B always stored
+    p[gOffset] = g;
+    p[bOffset] = b;
+  }
+}
+
+void NeoPixel_Simulator::setPixelColor(
+ uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+
+  if(n < numLEDs) {
+    if(brightness) { // See notes in setBrightness()
+      r = (r * brightness) >> 8;
+      g = (g * brightness) >> 8;
+      b = (b * brightness) >> 8;
+      w = (w * brightness) >> 8;
+    }
+    uint8_t *p;
+    if(wOffset == rOffset) { // Is an RGB-type strip
+      p = &pixels[n * 3];    // 3 bytes per pixel (ignore W)
+    } else {                 // Is a WRGB-type strip
+      p = &pixels[n * 4];    // 4 bytes per pixel
+      p[wOffset] = w;        // Store W
+    }
+    p[rOffset] = r;          // Store R,G,B
+    p[gOffset] = g;
+    p[bOffset] = b;
+  }
+}
+
+// Set pixel color from 'packed' 32-bit RGB color:
+void NeoPixel_Simulator::setPixelColor(uint16_t n, uint32_t c) {
+  if(n < numLEDs) {
+    uint8_t *p,
+      r = (uint8_t)(c >> 16),
+      g = (uint8_t)(c >>  8),
+      b = (uint8_t)c;
+    if(brightness) { // See notes in setBrightness()
+      r = (r * brightness) >> 8;
+      g = (g * brightness) >> 8;
+      b = (b * brightness) >> 8;
+    }
+    if(wOffset == rOffset) {
+      p = &pixels[n * 3];
+    } else {
+      p = &pixels[n * 4];
+      uint8_t w = (uint8_t)(c >> 24);
+      p[wOffset] = brightness ? ((w * brightness) >> 8) : w;
+    }
+    p[rOffset] = r;
+    p[gOffset] = g;
+    p[bOffset] = b;
+  }
+}
+
+// Convert separate R,G,B into packed 32-bit RGB color.
+// Packed format is always RGB, regardless of LED strand color order.
+uint32_t NeoPixel_Simulator::Color(uint8_t r, uint8_t g, uint8_t b) {
+  return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+}
+
+// Convert separate R,G,B,W into packed 32-bit WRGB color.
+// Packed format is always WRGB, regardless of LED strand color order.
+uint32_t NeoPixel_Simulator::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+  return ((uint32_t)w << 24) | ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+}
+
+// Query color from previously-set pixel (returns packed 32-bit RGB value)
+uint32_t NeoPixel_Simulator::getPixelColor(uint16_t n) const {
+  if(n >= numLEDs) return 0; // Out of bounds, return no color.
+
+  uint8_t *p;
+
+  if(wOffset == rOffset) { // Is RGB-type device
+    p = &pixels[n * 3];
+    if(brightness) {
+      // Stored color was decimated by setBrightness().  Returned value
+      // attempts to scale back to an approximation of the original 24-bit
+      // value used when setting the pixel color, but there will always be
+      // some error -- those bits are simply gone.  Issue is most
+      // pronounced at low brightness levels.
+      return (((uint32_t)(p[rOffset] << 8) / brightness) << 16) |
+             (((uint32_t)(p[gOffset] << 8) / brightness) <<  8) |
+             ( (uint32_t)(p[bOffset] << 8) / brightness       );
+    } else {
+      // No brightness adjustment has been made -- return 'raw' color
+      return ((uint32_t)p[rOffset] << 16) |
+             ((uint32_t)p[gOffset] <<  8) |
+              (uint32_t)p[bOffset];
+    }
+  } else {                 // Is RGBW-type device
+    p = &pixels[n * 4];
+    if(brightness) { // Return scaled color
+      return (((uint32_t)(p[wOffset] << 8) / brightness) << 24) |
+             (((uint32_t)(p[rOffset] << 8) / brightness) << 16) |
+             (((uint32_t)(p[gOffset] << 8) / brightness) <<  8) |
+             ( (uint32_t)(p[bOffset] << 8) / brightness       );
+    } else { // Return raw color
+      return ((uint32_t)p[wOffset] << 24) |
+             ((uint32_t)p[rOffset] << 16) |
+             ((uint32_t)p[gOffset] <<  8) |
+              (uint32_t)p[bOffset];
+    }
+  }
+}
+
+// Returns pointer to pixels[] array.  Pixel data is stored in device-
+// native format and is not translated here.  Application will need to be
+// aware of specific pixel data format and handle colors appropriately.
+uint8_t *NeoPixel_Simulator::getPixels(void) const {
+  return pixels;
+}
+
+uint16_t NeoPixel_Simulator::numPixels(void) const {
+  return numLEDs;
+}
+
+// Adjust output brightness; 0=darkest (off), 255=brightest.  This does
+// NOT immediately affect what's currently displayed on the LEDs.  The
+// next call to show() will refresh the LEDs at this level.  However,
+// this process is potentially "lossy," especially when increasing
+// brightness.  The tight timing in the WS2811/WS2812 code means there
+// aren't enough free cycles to perform this scaling on the fly as data
+// is issued.  So we make a pass through the existing color data in RAM
+// and scale it (subsequent graphics commands also work at this
+// brightness level).  If there's a significant step up in brightness,
+// the limited number of steps (quantization) in the old data will be
+// quite visible in the re-scaled version.  For a non-destructive
+// change, you'll need to re-render the full strip data.  C'est la vie.
+void NeoPixel_Simulator::setBrightness(uint8_t b) {
+  // Stored brightness value is different than what's passed.
+  // This simplifies the actual scaling math later, allowing a fast
+  // 8x8-bit multiply and taking the MSB.  'brightness' is a uint8_t,
+  // adding 1 here may (intentionally) roll over...so 0 = max brightness
+  // (color values are interpreted literally; no scaling), 1 = min
+  // brightness (off), 255 = just below max brightness.
+  uint8_t newBrightness = b + 1;
+  if(newBrightness != brightness) { // Compare against prior value
+    // Brightness has changed -- re-scale existing data in RAM
+    uint8_t  c,
+            *ptr           = pixels,
+             oldBrightness = brightness - 1; // De-wrap old brightness value
+    uint16_t scale;
+    if(oldBrightness == 0) scale = 0; // Avoid /0
+    else if(b == 255) scale = 65535 / oldBrightness;
+    else scale = (((uint16_t)newBrightness << 8) - 1) / oldBrightness;
+    for(uint16_t i=0; i<numBytes; i++) {
+      c      = *ptr;
+      *ptr++ = (c * scale) >> 8;
+    }
+    brightness = newBrightness;
+  }
+}
+
+//Return the brightness value
+uint8_t NeoPixel_Simulator::getBrightness(void) const {
+  return brightness - 1;
+}
+
+void NeoPixel_Simulator::clear() {
+  memset(pixels, 0, numBytes);
+}
+
+/* A PROGMEM (flash mem) table containing 8-bit unsigned sine wave (0-255).
+   Copy & paste this snippet into a Python REPL to regenerate:
+import math
+for x in range(256):
+    print("{:3},".format(int((math.sin(x/128.0*math.pi)+1.0)*127.5+0.5))),
+    if x&15 == 15: print
+*/
+static const uint8_t PROGMEM _sineTable[256] = {
+  128,131,134,137,140,143,146,149,152,155,158,162,165,167,170,173,
+  176,179,182,185,188,190,193,196,198,201,203,206,208,211,213,215,
+  218,220,222,224,226,228,230,232,234,235,237,238,240,241,243,244,
+  245,246,248,249,250,250,251,252,253,253,254,254,254,255,255,255,
+  255,255,255,255,254,254,254,253,253,252,251,250,250,249,248,246,
+  245,244,243,241,240,238,237,235,234,232,230,228,226,224,222,220,
+  218,215,213,211,208,206,203,201,198,196,193,190,188,185,182,179,
+  176,173,170,167,165,162,158,155,152,149,146,143,140,137,134,131,
+  128,124,121,118,115,112,109,106,103,100, 97, 93, 90, 88, 85, 82,
+   79, 76, 73, 70, 67, 65, 62, 59, 57, 54, 52, 49, 47, 44, 42, 40,
+   37, 35, 33, 31, 29, 27, 25, 23, 21, 20, 18, 17, 15, 14, 12, 11,
+   10,  9,  7,  6,  5,  5,  4,  3,  2,  2,  1,  1,  1,  0,  0,  0,
+    0,  0,  0,  0,  1,  1,  1,  2,  2,  3,  4,  5,  5,  6,  7,  9,
+   10, 11, 12, 14, 15, 17, 18, 20, 21, 23, 25, 27, 29, 31, 33, 35,
+   37, 40, 42, 44, 47, 49, 52, 54, 57, 59, 62, 65, 67, 70, 73, 76,
+   79, 82, 85, 88, 90, 93, 97,100,103,106,109,112,115,118,121,124};
+
+/* Similar to above, but for an 8-bit gamma-correction table.
+   Copy & paste this snippet into a Python REPL to regenerate:
+import math
+gamma=2.6
+for x in range(256):
+    print("{:3},".format(int(math.pow((x)/255.0,gamma)*255.0+0.5))),
+    if x&15 == 15: print
+*/
+static const uint8_t PROGMEM _gammaTable[256] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,
+    1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,  2,  3,  3,  3,  3,
+    3,  3,  4,  4,  4,  4,  5,  5,  5,  5,  5,  6,  6,  6,  6,  7,
+    7,  7,  8,  8,  8,  9,  9,  9, 10, 10, 10, 11, 11, 11, 12, 12,
+   13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20,
+   20, 21, 21, 22, 22, 23, 24, 24, 25, 25, 26, 27, 27, 28, 29, 29,
+   30, 31, 31, 32, 33, 34, 34, 35, 36, 37, 38, 38, 39, 40, 41, 42,
+   42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+   58, 59, 60, 61, 62, 63, 64, 65, 66, 68, 69, 70, 71, 72, 73, 75,
+   76, 77, 78, 80, 81, 82, 84, 85, 86, 88, 89, 90, 92, 93, 94, 96,
+   97, 99,100,102,103,105,106,108,109,111,112,114,115,117,119,120,
+  122,124,125,127,129,130,132,134,136,137,139,141,143,145,146,148,
+  150,152,154,156,158,160,162,164,166,168,170,172,174,176,178,180,
+  182,184,186,188,191,193,195,197,199,202,204,206,209,211,213,215,
+  218,220,223,225,227,230,232,235,237,240,242,245,247,250,252,255};
+
+uint8_t NeoPixel_Simulator::sine8(uint8_t x) const {
+  return pgm_read_byte(&_sineTable[x]); // 0-255 in, 0-255 out
+}
+
+uint8_t NeoPixel_Simulator::gamma8(uint8_t x) const {
+  return pgm_read_byte(&_gammaTable[x]); // 0-255 in, 0-255 out
+}
+
